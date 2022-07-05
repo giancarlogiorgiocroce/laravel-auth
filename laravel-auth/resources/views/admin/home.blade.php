dbd342afb42ea68f2da66a149135a5fd97d24f35
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-07-05 15:40:41 +0200

    Diviso view Guest da Admin e corrette le rotte

--- laravel-auth/resources/views/admin/home.blade.php
+++ laravel-auth/resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Sei loggato!
                 </div>
             </div>
         </div>
